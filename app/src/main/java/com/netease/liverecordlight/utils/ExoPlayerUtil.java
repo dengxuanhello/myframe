2a00fa4e26634a62ea178edcc9768d46dd91b32d
dengxuan <771757721@qq.com>
2017-07-15 14:04:18 +0800

    exoplayer

---
+++ app/src/main/java/com/netease/liverecordlight/utils/ExoPlayerUtil.java
@@ -0,0 +1,53 @@
+package com.netease.liverecordlight.utils;
+
+import android.content.Context;
+import android.net.Uri;
+import android.view.Surface;
+
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.extractor.ExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+
+/**
+ * Created by dengxuan on 2017/7/11.
+ */
+
+public class ExoPlayerUtil {
+
+    private SimpleExoPlayer simpleExoPlayer;
+    private Surface surface;
+
+    public void setUpExoPlayer(Context context, String videoUrl){
+        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        TrackSelection.Factory videoTrackSelectionFactory = new AdaptiveTrackSelection.Factory(bandwidthMeter);
+        TrackSelector trackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
+        // Measures bandwidth during playback. Can be null if not required.
+        DefaultBandwidthMeter defaultBandwidthMeter = new DefaultBandwidthMeter();
+        // Produces DataSource instances through which media data is loaded.
+        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(context, Util.getUserAgent(context, "yourApplicationName"), defaultBandwidthMeter);
+        // Produces Extractor instances for parsing the media data.
+        ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
+        // This is the MediaSource representing the media to be played.
+        MediaSource videoSource = new ExtractorMediaSource(Uri.parse(videoUrl), dataSourceFactory, extractorsFactory, null, null);
+        // SimpleExoPlayer
+        simpleExoPlayer = ExoPlayerFactory.newSimpleInstance(context, trackSelector);
+        // Prepare the player with the source.
+        simpleExoPlayer.prepare(videoSource);
+        //player.setVideoSurface(surface);
+        //player.setPlayWhenReady(true);
+    }
+
+}
